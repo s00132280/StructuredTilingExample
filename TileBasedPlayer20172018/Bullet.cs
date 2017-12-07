c1038658509ed685acc7245254405aaa45aae2ce
Stephen Mills <s00132280@mail.itsligo.ie>
2017-12-07 11:51:40 +0000

    added bullet class. dawn to screen at 128,128.

--- TileBasedPlayer20172018/Bullet.cs
+++ TileBasedPlayer20172018/Bullet.cs
@@ -1,22 +1,20 @@
 ﻿using AnimatedSprite;
+using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Xna.Framework;
 using Tiling;
-using Microsoft.Xna.Framework.Graphics;
-using Tiler;
 
 namespace TileBasedPlayer20172018
 {
-    class Projectile : RotatingSprite
+    class Bullet : RotatingSprite
     {
         //List<TileRef> images = new List<TileRef>() { new TileRef(15, 2, 0)};
         //TileRef currentFrame;
 
-        public Projectile(Game game, Vector2 userPosition,
+        public Bullet(Game game, Vector2 userPosition,
             List<TileRef> sheetRefs, int frameWidth, int frameHeight, float layerDepth) 
                 : base(game, userPosition, sheetRefs, frameWidth, frameHeight, layerDepth)
         {
